a6785402907c252336938bdbd5bdc88702ecb5cb
wait4ndbleed <73363131+wait4ndbleed@users.noreply.github.com>
2023-09-14 13:46:23 -0300

    disabled
    Adicionando disabled aos inputs e button

--- src/App.tsx
+++ src/App.tsx
@@ -45,15 +45,22 @@ const App = () => {
             placeholder="Digite a sua altura. Ex: 1.5 (em métros)"
             value={heightField > 0 ? heightField : ""}
             onChange={(e) => setHeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
           />
           <input
             type="number"
             placeholder="Digite a sua peso. Ex: 75.3 (em kg)"
             value={weightField > 0 ? weightField : ""}
             onChange={(e) => setWeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
           />
 
-          <button onClick={handleCaltulateButton}>Calcular</button>
+          <button
+            onClick={handleCaltulateButton}
+            disabled={toShow ? true : false}
+          >
+            Calcular
+          </button>
         </div>
         <div className={styles.rightSide}>
           {!toShow && (
